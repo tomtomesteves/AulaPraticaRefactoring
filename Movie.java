527b93c04411aba5629454faba2e71ecd71d22a4
thomas <thomasesteves95@hotmail.com>
2021-03-12 18:14:28 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -35,26 +35,6 @@ public class Movie {
       return _title;
   }
 
-  public double getCharge(int daysRented) {
-       double thisAmount = 0;
-       switch (getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (daysRented > 2)
-                 thisAmount += (daysRented - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += daysRented * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (daysRented > 3)
-                 thisAmount += (daysRented - 3) * 1.5;
-               break;
-        }
-        return thisAmount;
-   }
-
    public int getFrequentRenterPoints(int daysRented) {
       // add frequent renter points
       int frequentRenterPoints = 0;
@@ -64,4 +44,8 @@ public class Movie {
          daysRented > 1) frequentRenterPoints ++;
       return frequentRenterPoints;   
   }
+
+   public double getCharge(int daysRented) {
+         return _price.getCharge(daysRented);
+   }
 }
