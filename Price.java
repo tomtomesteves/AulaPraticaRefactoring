063f5081b19f4c3824554c15bb29fd2757624c28
thomas <thomasesteves95@hotmail.com>
2021-03-12 18:20:39 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -3,12 +3,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-      // add frequent renter points
-      int frequentRenterPoints = 0;
-      frequentRenterPoints++;
-      // add bonus for a two day new release rental
-      if ((getPriceCode() == Movie.NEW_RELEASE) &&
-         daysRented > 1) frequentRenterPoints ++;
-      return frequentRenterPoints;   
-  }
+       return 1;
+   }
 }
