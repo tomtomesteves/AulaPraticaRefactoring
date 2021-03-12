6722408778f1e0ca23f676d47c7e31117f2df247
thomas <thomasesteves95@hotmail.com>
2021-03-12 17:46:25 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -26,7 +26,7 @@ public class Customer {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
 
-        thisAmount = amountFor(each);
+        thisAmount = each.getCharge();
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
@@ -46,23 +46,4 @@ public class Customer {
      return result;
    }
 
-   private double amountFor(Rental aRental) {
-       double thisAmount = 0;
-       switch (aRental.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (aRental.getDaysRented() > 2)
-                 thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += aRental.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (aRental.getDaysRented() > 3)
-                 thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-               break;
-        }
-        return thisAmount;
-   }
 }
